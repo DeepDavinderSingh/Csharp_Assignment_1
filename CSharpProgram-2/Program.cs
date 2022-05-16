6d2fe8c8ab92ee8b793d301480487e9ba4f8f902
Davinder Singh <davinder@lngconsultancy.co.in>
2022-05-16 14:28:28 +0530

    Added Solution of Second Program

---
+++ CSharpProgram-2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class program
+{
+    public static void Main()
+    {
+        //Console.WriteLine("Enter the number:");
+        //int n = Convert.ToInt32(Console.ReadLine());
+
+        //int i, j, k;
+
+        //for (i = 1; i <= n; i++)
+        //{
+        //        for (k = 1; k <= n - i; k++)
+        //        {
+        //            Console.Write(" ");
+        //        }
+        //        for (j = 1; j <= i; j++)
+        //        {
+        //            Console.Write("* ");
+        //        }
+
+        //    Console.WriteLine();
+        //}
+
+
+        int _tableOf;
+        Console.WriteLine("Table of:");
+        _tableOf = Convert.ToInt32(Console.ReadLine());
+
+        int _tableUpto;
+        Console.WriteLine("Table Upto:");
+        _tableUpto = Convert.ToInt32(Console.ReadLine());
+
+        int i = 1;
+
+        while (i <= _tableUpto)
+        {
+            Console.WriteLine(_tableOf + "x" + i + "=" + _tableOf * i);
+            i++;
+        }
+
+        
+
+        //for (int i = 1; i <=_tableUpto; i++)
+        //{
+        //    Console.WriteLine(_tableOf + "x"+ i + "=" + _tableOf * i);
+        //}
+
+
+        //Console.WriteLine("Enter the number:");
+        //int n = Convert.ToInt32(Console.ReadLine());
+
+        //void int Sum()
+        //{
+        //    for (int i = 0; i < n; i++) ;
+        //    {
+        //        int sum = n % 10;
+        //    }
+        //}
+
+    }
+    
+}
