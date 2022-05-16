52342830c79c44b6c84461063c7da974abfdfdd9
Davinder Singh <davinder@lngconsultancy.co.in>
2022-05-16 13:12:14 +0530

    intial commit

---
+++ CSharpAssignment-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class program 
+{ 
+    public static void Main()
+    {
+        Console.WriteLine("Enter the number:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the width:");
+        int width = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= width; i++)
+        {
+            for (int j = width; j >= i; j--)
+            {
+                Console.Write(n);
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
